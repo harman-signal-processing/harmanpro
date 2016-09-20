ef54c60b45bf190f05b6a4a0b86775a540c8c74f
Adam Anderson <adam@makeascene.com>
2016-09-20 08:17:03 -0600

    Finance calc tweakin

--- app/controllers/finance_controller.rb
+++ app/controllers/finance_controller.rb
@@ -1,11 +1,5 @@
 class FinanceController < ApplicationController
   def index
-    if LandingPage.exists?(slug: 'finance')
-      render_landing_page('finance')
-    elsif LandingPage.exists?(slug: 'financing')
-      render_landing_page('financing')
-    else
-      render_landing_page('harman-equipment-financing')
-    end
+    render_landing_page('finance')
   end
 end
