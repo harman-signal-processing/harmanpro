4b7bcf1fd97ea3c82c9019452df00a0d2ac1f4e6
Adam Anderson <adam@makeascene.com>
2016-11-14 14:06:10 -0700

    adding vm background to api

--- app/serializers/vertical_market_serializer.rb
+++ app/serializers/vertical_market_serializer.rb
@@ -1,9 +1,17 @@
 class VerticalMarketSerializer < ActiveModel::Serializer
-  attributes :id, :name, :headline, :description, :banner_url, :slug, :parent_id
+  attributes :id, :name, :headline, :description, :banner_url, :slug, :parent_id, :background_url
   has_many :reference_systems
 
   def banner_url
     ActionController::Base.helpers.asset_path object.banner.url(:large)
+  end
+
+  def background_url
+    if object.background.present?
+      ActionController::Base.helpers.asset_path object.background.url
+    else
+      ""
+    end
   end
 
   def reference_systems
