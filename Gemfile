00e6db2f83304a5e4b44b3e3af3bead3a50bf36c
Adam Anderson <adam@makeascene.com>
2017-02-09 15:13:13 -0700

    Switching leadgen form to silverpop

--- Gemfile
+++ Gemfile
@@ -39,7 +39,9 @@ gem 'delayed_job_active_record'
 gem 'daemons'
 gem 'httparty'
 gem 'rails_autolink'
-gem 'cheetah_mail', "~> 0.6.0"
+gem 'silverpop'
+gem 'hashie', '~> 3.4.6' # 3.5.1 was causing errors with some silverpop transactions
+gem 'oauth2'
 gem 'ransack' # using for service center search
 gem 'exception_notification'
 gem 'sdoc', '~> 0.4.0',          group: :doc
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -7,18 +7,20 @@ class Lead < ActiveRecord::Base
   attr_accessor :locale
 
   def subscribe!
-    @cheetahmail_client ||= CheetahMail::CheetahMail.new({
-      :host      => 'ebm.cheetahmail.com',
-      :username  => ENV['cheetahmail_username'],
-      :password  => ENV['cheetahmail_password'],
-      :aid       => ENV['cheetahmail_aid'],
-      :messenger => CheetahMail::SynchronousMessenger
-    })
-
     begin
-      @cheetahmail_client.mailing_list_update(self.email, sub: ENV['cheetahmail_sub'])
-    rescue CheetahMailException
-      logger.debug("There was some cheetahmail exception")
+      name_fields = name.split(/\s/)
+      user_params = {
+        email: email,
+        :"BR_HPro" => true,
+        :"First Name" => name_fields.first,
+        :"Last Name" => name_fields.last,
+        :country => location,
+        :company => company,
+        :phone => phone
+      }
+      silverpop_client.add_recipient(user_params, ENV['SILVERPOP_DB_ID'], [ENV['SILVERPOP_LIST_ID']])
+    rescue
+      logger.debug("There was some silverpop exception")
     end
   end
   handle_asynchronously :subscribe!
@@ -26,4 +28,18 @@ class Lead < ActiveRecord::Base
   def notify_leadgen_recipients
     LeadMailer.new_lead(self, self.locale.to_s).deliver_later
   end
+
+  private
+
+  def silverpop_client
+    @silverpop_client ||= SilverPop.new(access_token: silverpop_access_token.token, url: ENV['SILVERPOP_API_URL'])
+  end
+
+  def silverpop_access_token
+    client = OAuth2::Client.new(ENV['SILVERPOP_CLIENT_ID'],
+                                ENV['SILVERPOP_CLIENT_SECRET'],
+                                site: "#{ENV['SILVERPOP_API_URL']}/oauth/token")
+    @silverpop_access_token ||= OAuth2::AccessToken.from_hash(client, refresh_token: ENV['SILVERPOP_REFRESH_TOKEN']).refresh!
+  end
+
 end
