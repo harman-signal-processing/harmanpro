ac5b151a15833ea18cd886a2eb0adb3bbb99f2bf
Adam Anderson <adam@makeascene.com>
2017-02-22 11:51:00 -0700

    Moving the HEF box on vertical market pages

--- spec/views/vertical_markets/_learn.html.erb_spec.rb
+++ spec/views/vertical_markets/_learn.html.erb_spec.rb
@@ -23,4 +23,28 @@ RSpec.describe "vertical_markets/_learn.html.erb", :type => :view do
     expect(rendered).to have_link(@case_study.headline, href: case_study_path(@case_study))
   end
 
+  context "non-retail" do
+    before do
+      @parent_vertical_market = FactoryGirl.create(:vertical_market)
+      @vertical_market.update_column(:parent_id, @parent_vertical_market.id)
+      @vertical_market.update_column(:retail, false)
+      FactoryGirl.create(:site_setting, name: "hef-sidebar-title", content: "HEF Headline")
+      FactoryGirl.create(:site_setting, name: "hef-description", content: "HEF description")
+      FactoryGirl.create(:site_setting, name: "subheader-help-find-installer", content: "Help Me Find Installer")
+      FactoryGirl.create(:site_setting, name: "help-find-installer-description", content: "Paragraph helping customer find installer.")
+      FactoryGirl.create(:site_setting, name: "button-help-find-installer", content: "Help Me Find Installer")
+      FactoryGirl.create(:site_setting, name: "hef-link", content: "www.hef.com")
+      render partial: "vertical_markets/learn", locals: { vertical_market: @vertical_market }
+    end
+
+    it "shows HEF content" do
+      expect(rendered).to have_css("h4", text: "HEF Headline")
+      expect(rendered).to have_content("HEF description")
+    end
+
+    it "links to learn more about HEF" do
+      expect(rendered).to have_link("Learn More", href: "www.hef.com")
+    end
+  end
+
 end
