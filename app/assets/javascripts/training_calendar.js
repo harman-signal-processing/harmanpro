cc626bb6295e69460382359eedbf0ae7f3b51e89
Darryl Dalton <darryl.dalton@gmail.com>
2017-04-06 19:05:48 +0000

    Add Math.sign polyfill for training_calendar.js

--- app/assets/javascripts/training_calendar.js
+++ app/assets/javascripts/training_calendar.js
@@ -390,6 +390,23 @@ $(function ()
         return Math.sign(new Date(b.StartDate) - new Date(a.StartDate));
     }  //  function sortCourseStartDesc(a, b)
 
+    //  Math.sign Polyfill for stupid IE
+    if (!Math.sign) 
+    {
+        Math.sign = function(x) 
+        {
+            // If x is NaN, the result is NaN.
+            // If x is -0, the result is -0.
+            // If x is +0, the result is +0.
+            // If x is negative and not -0, the result is -1.
+            // If x is positive and not +0, the result is +1.
+            x = +x; // convert to a number
+            if (x === 0 || isNaN(x)) {
+            return Number(x);
+            }
+            return x > 0 ? 1 : -1;
+        };
+    }
 
 });  //  $(function ()
 
