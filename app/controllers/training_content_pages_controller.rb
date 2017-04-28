8d52831c2e19361f950f45d574742a628000e80e
Darryl Dalton <darryl.dalton@gmail.com>
2017-04-28 22:06:26 +0000

    Added redirect to training home to remove ud parameter

--- app/controllers/training_content_pages_controller.rb
+++ app/controllers/training_content_pages_controller.rb
@@ -1,6 +1,12 @@
 class TrainingContentPagesController < ApplicationController
   def show
     slug = params[:id].nil? ? "training-home" : params[:id]
+    
+    #This is a total hack to hide the ud parameter
+    if params[:ud].present? && slug == "training-home"
+      redirect_to "/training" and return false
+    end
+    
     render_training_content_page(slug)
   end # def show
 
