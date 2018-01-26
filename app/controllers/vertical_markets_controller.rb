c7f4672f2c4db1cfad55a18aa57ff0402a42869a
Adam Anderson <adam@makeascene.com>
2018-01-26 10:34:43 -0700

    Inactive verticals only accessible by admins

--- app/controllers/vertical_markets_controller.rb
+++ app/controllers/vertical_markets_controller.rb
@@ -13,7 +13,15 @@ class VerticalMarketsController < ApplicationController
   def show
     @vertical_market = VerticalMarket.find(params[:id])
     @lead = Lead.new(vertical_market: @vertical_market)
-    respond_with @vertical_market
+    if @vertical_market.live? || special_access_granted?
+      respond_with @vertical_market
+    else
+      redirect_to vertical_markets_path and return false
+    end
+  end
+
+  def special_access_granted?
+    user_signed_in? && (current_user.admin_access? || curent_user.cms_user?)
   end
 
 end
