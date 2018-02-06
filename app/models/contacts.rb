b1815fd497d2719a33ac9b5fa31983e6e2454daa
Darryl Dalton <darryl.dalton@gmail.com>
2018-02-06 22:45:10 +0000

    Cleaning up and adding new method get_all_contact_nav_list to Contacts model

--- app/models/contacts.rb
+++ app/models/contacts.rb
@@ -1,32 +1,33 @@
 class Contacts
-    # include HTTParty
-    # base_uri 'https://trade.amx.com/_contactsTest'
     
     def self.get_all_contacts
-        # contacts = get('/contacts.json')
-        
         contact_data_file_path = Rails.root.join("public/contacts.json")
         contact_data_file_timestamp = File.mtime(contact_data_file_path)
-
         cache_key = ["contacts.cache", contact_data_file_timestamp].join('-')
         Rails.cache.fetch(cache_key, expires_in: 1.day) do
             contacts = File.read(contact_data_file_path)
             JSON.parse(contacts)
         end
-        
     end  #  def get_all_contacts
     
     def self.get_all_contact_slugs
-        
         contact_data_file_path = Rails.root.join("public/contact.slugs.json")
         contact_data_file_timestamp = File.mtime(contact_data_file_path)
-
         cache_key = ["contact.slugs.cache", contact_data_file_timestamp].join('-')
         Rails.cache.fetch(cache_key, expires_in: 1.day) do
             contacts = File.read(contact_data_file_path)
             JSON.parse(contacts)
         end        
-        
     end  #  def self.get_all_contact_slugs
+    
+    def self.get_all_contact_nav_list
+        contact_data_file_path = Rails.root.join("public/contacts.nav.json")
+        contact_data_file_timestamp = File.mtime(contact_data_file_path)
+        cache_key = ["contacts.nav.cache", contact_data_file_timestamp].join('-')
+        Rails.cache.fetch(cache_key, expires_in: 1.day) do
+            contacts = File.read(contact_data_file_path)
+            JSON.parse(contacts)
+        end        
+    end  #  def self.get_all_contact_nav_list    
     
 end  # class Contacts
