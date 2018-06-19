7c495201bf8d25a79e53a669f0e1abdb550ff5ec
Adam Anderson <adam@makeascene.com>
2018-06-19 15:08:12 -0600

    Gem updates

--- spec/views/cms/index.html.erb_spec.rb
+++ spec/views/cms/index.html.erb_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "cms/index.html.erb" do
   end
 
   it "shows translations needed for locale corresponding to user" do
-    expect(rendered).to have_content("items to be translated for #{ @locale.name}")
+    expect(rendered.to_s.gsub(/\s+/, ' ')).to have_text("items to be translated for #{ @locale.name}")
     expect(rendered).to have_link "start translating", href: cms_available_locale_path(@locale)
   end
 
