b1ec3ece364b5619afdb9308ea01991b1d8a425d
Adam Anderson <adam@makeascene.com>
2018-11-12 10:13:35 -0700

    Ruby version up to 2.4.5, Gem updates

--- spec/models/shorturl_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Shorturl, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/vertical_markets/_resource_library_panel.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "vertical_markets/_resource_library_panel.html.erb", :type => :view do
-  it "renders related resource library content"
-end
