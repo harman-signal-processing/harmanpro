bc0826288862a990349adc8b3afbe2ce6e08e903
Darryl Dalton <darryl.dalton@gmail.com>
2019-04-18 19:58:59 +0000

    Fixes for LocationInfo admin pages

--- app/controllers/location_info/admin/regions_controller.rb
+++ app/controllers/location_info/admin/regions_controller.rb
@@ -7,17 +7,28 @@ class LocationInfo::Admin::RegionsController < LocationInfo::AdminController
   
   def new
     @region = LocationInfo::Region.new
+    # binding.pry
+    location_id = params[:region][:location_id] unless params[:region].nil?
+    @location = LocationInfo::Location.find(location_id) if location_id.present?    
+    
   end
   
   def edit
     @location_region = LocationInfo::LocationRegion.new(location_info_region_id: @region.id)
-    @location_regions = LocationInfo::LocationRegion.where(location_info_region_id: @region.id)     
+    @location_regions = LocationInfo::LocationRegion.where(location_info_region_id: @region.id)    
   end
   
   def create
-    @region = LocationInfo::Region.new(region_params)
+    @region = LocationInfo::Region.new(region_params.except(:location_id))
+    @location = region_params[:location_id].present? ? LocationInfo::Location.find(region_params[:location_id]) : nil
+    
     if @region.save
-      redirect_to location_info_admin_regions_path, notice: "The region #{@region.name} was created successfully."
+      if @location.present?
+        @region.locations << @location
+        redirect_to edit_location_info_admin_location_path(@location), notice: "The region #{@region.name} was created successfully and associated to #{@location.name}."      
+      else
+        redirect_to location_info_admin_regions_path, notice: "The region #{@region.name} was created successfully."
+      end
     else
       render action: :new
     end
@@ -42,7 +53,7 @@ class LocationInfo::Admin::RegionsController < LocationInfo::AdminController
   end  
   
   def region_params
-    params.require(:location_info_region).permit(:name)
+    params.require(:location_info_region).permit(:name, :location_id)
   end   
   
 end  #  class LocationInfo::Admin::RegionsController < LocationInfo::AdminController
