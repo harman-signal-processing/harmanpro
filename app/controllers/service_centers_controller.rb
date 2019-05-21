324947db81353da245f33410eab5cdecb5251491
Darryl Dalton <darryl.dalton@gmail.com>
2019-05-21 16:12:52 +0000

    New method to retrieve service centers for brand/state

--- app/controllers/service_centers_controller.rb
+++ app/controllers/service_centers_controller.rb
@@ -1,4 +1,5 @@
 class ServiceCentersController < ApplicationController
+  respond_to :json
 
   # GET /service_centers
   # GET /service_centers.json
@@ -41,7 +42,49 @@ class ServiceCentersController < ApplicationController
     end
   end
 
+  def service_centers_for_brand
+    brand = params[:brand].nil? ? "bss" : params[:brand]
+    brand_id = Brand.find_by_name(brand).id
+    state = params[:state].nil? ? "any" : params[:state]
+    service_groups = ServiceGroup.where("brand_id = ?", brand_id).uniq
+    if state == "any"
+      service_centers = ServiceCenter.joins(:service_groups).where("service_group_id in (?) and active = true",service_groups.pluck(:id))
+    else
+      service_centers = ServiceCenter.joins(:service_groups).where("service_group_id in (?) and state = ? and active = true",service_groups.pluck(:id), state)
+    end
+    
+    result = service_centers.as_json(
+       except: [:created_at, :updated_at],  # exclude these service center attributes
+       include: { 
+         service_groups: { 
+           only:[:id, :name, ],
+           include: { 
+             brand: {
+               only:[:id, :name]
+             }  #  brand
+           }  #  service groups include
+         }  # service groups
+       },  # service centers include
+      )  #  result = service_groups.as_json(
+      
+    # Will need to filter out service groups that don't match brand
+    filtered_result = remove_service_groups_not_matching_brand(result, brand_id)
+      
+    respond_with filtered_result
+    
+  end  #  def service_centers_for_brand    
+
   private
+
+  def remove_service_groups_not_matching_brand(service_centers, brand_id)
+    service_centers_and_service_groups_filtered_by_brand = service_centers.each do |service_center|
+      # removing service groups not associated to brand
+      service_center["service_groups"].delete_if{|service_group|
+        service_group["brand"]["id"] != brand_id
+      }  #  service_center["service_groups"].delete_if{|service_group|
+    end  #  service_centers_and_service_groups_filtered_by_brand = service_centers.each do |sc|
+    service_centers_and_service_groups_filtered_by_brand
+  end  #  def remove_service_groups_not_matching_brand(service_centers, brand_id)
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def service_center_params
