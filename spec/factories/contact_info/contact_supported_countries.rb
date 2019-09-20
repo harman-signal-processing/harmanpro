6d79b977b26491010f908409620ffaa0d52dedc8
Darryl Dalton <darryl.dalton@gmail.com>
2019-09-20 21:18:12 +0000

    Fixing broken rspec tests due to country harman_name requirement

--- spec/factories/contact_info/contact_supported_countries.rb
+++ spec/factories/contact_info/contact_supported_countries.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :contact_info_contact_supported_country, class: 'ContactInfo::ContactSupportedCountry' do
     contact { ContactInfo::Contact.create(name: 'Contact 1') }
-    country { LocationInfo::Country.create(name: 'Country 1') }
+    country { LocationInfo::Country.create(name: 'Country 1', harman_name: 'Country 1') }
     position { 1 }     
   end
 end
--- spec/factories/distributor_info/distributor_countries.rb
+++ spec/factories/distributor_info/distributor_countries.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :distributor_info_distributor_country, class: 'DistributorInfo::DistributorCountry' do
     distributor { DistributorInfo::Distributor.create(name: 'Distributor 1') }
-    country { LocationInfo::Country.create(name: 'Country 1') }
+    country { LocationInfo::Country.create(name: 'Country 1', harman_name: 'Country 1') }
     position { 1 }    
   end
 end
