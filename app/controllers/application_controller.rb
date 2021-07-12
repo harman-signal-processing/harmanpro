9527d136ca2eefa6421a40d37e55a43d835cedd6
Adam Anderson <adam@makeascene.com>
2021-07-12 14:00:51 -0600

    Adding local addressable gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
   before_action :set_locale, :save_passed_in_amx_trade_site_user_in_session_cookie
   before_action :configure_permitted_parameters, if: :devise_controller?
 
