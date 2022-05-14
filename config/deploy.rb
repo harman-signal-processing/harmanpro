8024a60462709493b74b18a005af72865271030a
Adam Anderson <adam@makeascene.com>
2022-05-14 14:32:36 +0000

    Gem updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'https://github.com/harman-signal-processing/harmanpro.git'
 
 set :ssh_options, compression: false, keepalive: true
 # Default branch is :master
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+#ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
