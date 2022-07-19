c15ecac7ef8a767850fdbf9afbc4db7f5185bfaf
Adam Anderson <adam@makeascene.com>
2022-07-19 22:55:42 +0000

    Fixing redirect error with allow_other_host: true

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,7 @@ class ResourcesController < ApplicationController
 
   def show
     @resource = Resource.find params[:id]
-    redirect_to @resource.attachment.url, status: 301 and return false
+    redirect_to @resource.attachment.url, status: 301, allow_other_host: true and return false
   end
 
 end
