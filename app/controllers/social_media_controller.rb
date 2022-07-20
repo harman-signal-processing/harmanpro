e6c56f9afd5335e15e370824c13126c4454b0f38
Adam Anderson <adam@makeascene.com>
2022-07-20 14:35:40 +0000

    Fixing more redirects with allow_other_host: true

--- app/controllers/social_media_controller.rb
+++ app/controllers/social_media_controller.rb
@@ -1,30 +1,30 @@
 class SocialMediaController < ApplicationController
   def facebook
-    redirect_to SiteSetting.value("facebook_link")
+    redirect_to SiteSetting.value("facebook_link"), allow_other_host: true
   end
   
   def twitter
-    redirect_to SiteSetting.value("twitter_link")
+    redirect_to SiteSetting.value("twitter_link"), allow_other_host: true
   end
   
   def linkedin
-    redirect_to SiteSetting.value("linkedin_link")
+    redirect_to SiteSetting.value("linkedin_link"), allow_other_host: true
   end
   
   def youtube
-    redirect_to SiteSetting.value("youtube_link")
+    redirect_to SiteSetting.value("youtube_link"), allow_other_host: true
   end
   
   def pinterest
-    redirect_to SiteSetting.value("pinterest_link")
+    redirect_to SiteSetting.value("pinterest_link"), allow_other_host: true
   end
   
   def google
-    redirect_to SiteSetting.value("google_link")
+    redirect_to SiteSetting.value("google_link"), allow_other_host: true
   end
   
   def instagram
-    redirect_to SiteSetting.value("instagram_link")
+    redirect_to SiteSetting.value("instagram_link"), allow_other_host: true
   end
   
 end  #  class SocialMediaController < ApplicationController
