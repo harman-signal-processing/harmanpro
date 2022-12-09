dec6b911a1bf7e0f0c0204f3316d772e340042ab
Darryl Dalton <darryl.dalton@gmail.com>
2022-12-09 17:06:41 -0600

    Updating pdf api to provide simple text list

--- app/controllers/api/v1/pdfs_controller.rb
+++ app/controllers/api/v1/pdfs_controller.rb
@@ -9,13 +9,16 @@ module Api
         @pdfs_to_index = @pdfs.map{|item|
           {id: item.id, name: item.name, file: item.attachment_file_name, url: item.attachment.url}
         }.uniq
+        @pdfs_for_text_list = @pdfs_to_index.pluck(:url)
 
         respond_to do |format|
           format.html
           format.xml { render xml: @pdfs_to_index }
           format.json { render json: { "pdfs" => @pdfs_to_index } }
+          format.text
         end
-      end
+
+      end  #  def index
 
     end  #  class PdfsController < ApplicationController
   end  #  module V1
