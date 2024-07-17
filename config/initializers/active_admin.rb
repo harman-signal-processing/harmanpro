20b8c78bcd877ffb22dfa33c4a20d1e2a01d80e2
Darryl Dalton <darryl.dalton@gmail.com>
2024-07-17 14:03:50 -0500

    Security update

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -145,6 +145,7 @@ ActiveAdmin.setup do |config|
   # Active Admin resources and pages from here.
   #
   # config.before_action :do_something_awesome
+   config.skip_before_action :catch_criminals
 
 
   # == Setting a Favicon
